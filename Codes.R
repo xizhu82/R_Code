cff765b643647eee3bcada84b273481302c13443
송석주 <songseogju@xizhumini.local>
2024-09-16 18:04:45 +0900

    New File

---
+++ Codes.R
@@ -0,0 +1,43 @@
+
+
+
+#### 크롤링 #####
+
+
+
+
+##### 정규식 #####
+Pitcher_data_fin$Date <- as.Date(str_extract(Pitcher_data_fin$Date, "^[^ ]+")) # Date 열을 날짜 형식으로 변환하면서 공백으로 분리된 첫 번째 부분만 추출
+
+
+##### 데이터 전처리 #####
+
+# 이동평균 #
+Starter_data <- Starter_data %>%
+  group_by(`Season`,Pitching) %>%
+  mutate(across(.cols = c(IP,ER,luck,WHIP,FIP,BBK,IPHR),
+                .fns = list(
+                  cumsum = ~ slide_dbl(., sum, .before = 12, .complete = FALSE) ,
+                  movavg = ~ slide_dbl(., mean, .before = 6, .complete = FALSE)  # 이동 평균 계산
+                )))
+
+# 비율 계산을 위해 mutate와 across 사용 #
+Starter_data <- Starter_data %>%
+  group_by(Season,Pitching) %>%
+  mutate(across(.cols = c("ER_cumsum","luck_cumsum","BBK_cumsum"),
+                .fns = ~ ./IP_cumsum,
+                .names = "{.col}_IP"))  # 생성될 새 열 이름 형식 지정
+
+Starter_data <- Starter_data %>%
+  group_by(Season,Pitching) %>%
+  # 지연(lag) 처리가 필요한 모든 열에 대해 지연 처리 적용
+  mutate(across(.cols = c(ends_with("_IP"),WHIP_cumsum,WHIP_movavg,FIP_cumsum,FIP_movavg,IPHR_cumsum,IPHR_movavg), 
+                .fns = ~ lag(., 1)))
+
+MLB_Reliver <- MLB_Reliver[, !names(MLB_Reliver) %in% text_columns] # 텍스트 열 제거
+
+numeric_indices <- sapply(MLB, is.numeric) # 숫자형 변수의 인덱스 찾기
+cols_to_select <- c(6:112) # 선택할 열의 범위를 정의
+numeric_cols_to_process <- MLB[, numeric_indices & colnames(MLB) %in% colnames(MLB)[cols_to_select]] # 해당 열의 숫자형 변수만 선택
+
+
